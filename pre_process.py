bec276ae049f25e3fbbd7b8a394ebcb4460a3ec4
calebkbrad <92956463+calebkbrad@users.noreply.github.com>
2022-10-20 17:46:10 -0400

    documentation

--- pre_process.py
+++ pre_process.py
@@ -16,6 +16,14 @@ def process_data(valid_input_file: str) -> tuple:
     Processes temperature data in an input file into a tuple where
     the first element is the list of times and the second element
     is a list containing a list for each core
+
+    Args:
+        valid_input_file (str): a valid file path to an input file with CPU temps for
+        four cores
+
+    Returns:
+        A tuple with a list of times as the first element, and a list of lists containing
+        data for each core
     """
     with open(valid_input_file, 'r') as temps_file:
             times = [0]
