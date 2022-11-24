a0ad0fa849de1509ef70134a3a4a248c41d9301f
cs_cbrad022 <92956463+calebkbrad@users.noreply.github.com>
2022-11-23 23:42:35 -0500

    add documentation

--- least_squares.py
+++ least_squares.py
@@ -9,12 +9,18 @@ Uses C0 and C1 formula derived in class to bypass need for a matrix solver
 
 from pre_process import process_data
 
-"""
-Compute the sums needed to plug in for C0 and C1 equations
+def compute_sums(times: list[int], temps: list[int]) -> tuple:
 
+    """
+    Compute the sums needed to plug in for C0 and C1 equations
 
-"""
-def compute_sums(times: list[int], temps: list[int], core_num: int):
+    Args:
+        times (list): List of all time (x) values    
+        temps (list): List of all corresponding temp (f(x)) values
+
+    Returns:
+        A tuple containing all 4 sums required for least squares computation
+    """
     x_sum = 0.0
     x_sq_sum = 0.0
     f_sum = 0.0
@@ -32,11 +38,11 @@ def compute_sums(times: list[int], temps: list[int], core_num: int):
     for time, temp in zip(times, temps):
         xf_sum += time * temp
     
-    return x_sum, x_sq_sum, f_sum, xf_sum
+    return (x_sum, x_sq_sum, f_sum, xf_sum)
 
 def main(argv):
     times, cores_data = process_data(argv[0])
-    x_sum, x_sq_sum, f_sum, xf_sum = compute_sums(times, cores_data[0], 0)
+    x_sum, x_sq_sum, f_sum, xf_sum = compute_sums(times, cores_data[0])
     print(x_sum)
     print(x_sq_sum)
     print(f_sum)
